5dd9b096cb85d54eb01fda3e3d2f58fb16350110
TinySymphony <zjutiny@gmail.com>
2017-05-01 23:24:53 +0800

    feat(Drawer): add disabled prop & coverage 100% :tada:

--- components/Drawer/Animation.js
+++ components/Drawer/Animation.js
@@ -14,10 +14,11 @@ Animation.prototype.animate = function (now) {
     end,
     duration,
     onAnimationFrame,
-    onAnimationEnd = () => {},
-    easingFunc = t => t
+    onAnimationEnd,
+    easingFunc = this.defaultEasing
   } = this.option;
-  var currentDuration = now - this.startTime;
+
+  let currentDuration = now - this.startTime;
   if (currentDuration >= duration) {
     onAnimationFrame(end);
     onAnimationEnd();
@@ -33,7 +34,9 @@ Animation.prototype.animate = function (now) {
   requestAnimationFrame(this.animate);
 };
 
-Animation.prototype.start = function () {
+Animation.prototype.start = function (time) {
   this.startTime = new Date();
-  this.animate(this.startTime);
+  this.animate(time || this.startTime);
 };
+
+Animation.prototype.defaultEasing = t => t;
